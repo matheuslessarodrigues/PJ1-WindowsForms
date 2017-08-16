ea98b82f964ff1092bbfc1676b54a9ca2bccace4
Matheus Lessa Rodrigues <matheus.lessa@cejoseleitelopes.com.br>
2017-08-16 12:15:59 -0300

    fazendo a lógica do Campo Minado manero

--- WindowsFormsTest/Form1.cs
+++ WindowsFormsTest/Form1.cs
@@ -12,28 +12,48 @@ namespace WindowsFormsTest
 {
     public partial class Form1 : Form
     {
+        private Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            textBox1.AppendText("\n");
-            textBox1.AppendText("AEEE " + trackBar1.Value);
-
-
-            checkBox1.Enabled = !checkBox1.Enabled;
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.Text = "OI, EU SOU O GOKU";
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            label.Text = trackBar1.Value.ToString();
+        }
+
+        private void LogicaMina(Button button)
+        {
+            if (rand.Next(2) == 0)
+                button.Text = "Mina";
+            else
+                button.Text = "Limpo";
+            button.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogicaMina(button1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LogicaMina(button2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LogicaMina(button3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LogicaMina(button4);
         }
     }
 }
